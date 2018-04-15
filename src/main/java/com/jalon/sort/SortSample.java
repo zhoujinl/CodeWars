4d7534ff338ce4d33189703889e9fd7cfd0afc97
zhoujinl <zhoujinl@126.com>
2018-04-15 17:05:34 +0800

    gbk2utf-8

--- src/main/java/com/jalon/sort/SortSample.java
+++ src/main/java/com/jalon/sort/SortSample.java
@@ -19,12 +19,12 @@ public class SortSample {
     }
 
     /*
- * ð������  ������ð����ȥ
- * �Ƚ����ڵ�Ԫ�ء������һ���ȵڶ����󣬾ͽ�������������
- * ��ÿһ������Ԫ����ͬ���Ĺ������ӿ�ʼ��һ�Ե���β�����һ�ԡ�����һ�㣬����Ԫ��Ӧ�û�����������
- * ������е�Ԫ���ظ����ϵĲ��裬�������һ����
- * ����ÿ�ζ�Խ��Խ�ٵ�Ԫ���ظ�����Ĳ��裬ֱ��û���κ�һ��������Ҫ�Ƚϡ�
- * @param numbers ��Ҫ�������������
+ * 冒泡排序  逐个逐个冒泡上去
+ * 比较相邻的元素。如果第一个比第二个大，就交换他们两个。
+ * 对每一对相邻元素作同样的工作，从开始第一对到结尾的最后一对。在这一点，最后的元素应该会是最大的数。
+ * 针对所有的元素重复以上的步骤，除了最后一个。
+ * 持续每次对越来越少的元素重复上面的步骤，直到没有任何一对数字需要比较。
+ * @param numbers 需要排序的整型数组
  */
     public static void bubbleSort(int[] numbers)
     {
@@ -36,7 +36,7 @@ public class SortSample {
             //System.out.println();
             for(int j = 0 ;j < size-1-i ; j++)
             {
-                if(numbers[j] > numbers[j+1])  //��������λ��
+                if(numbers[j] > numbers[j+1])  //交换两数位置
                 {
                     temp = numbers[j];
                     numbers[j] = numbers[j+1];
@@ -46,15 +46,15 @@ public class SortSample {
         }
     }
     /**
-     * ��������
+     * 插入排序
      *
-     * �ӵ�һ��Ԫ�ؿ�ʼ����Ԫ�ؿ�����Ϊ�Ѿ�������
-     * ȡ����һ��Ԫ�أ����Ѿ������Ԫ�������дӺ���ǰɨ��
-     * �����Ԫ�أ������򣩴�����Ԫ�أ�����Ԫ���Ƶ���һλ��
-     * �ظ�����3��ֱ���ҵ��������Ԫ��С�ڻ��ߵ�����Ԫ�ص�λ��
-     * ����Ԫ�ز��뵽��λ����
-     * �ظ�����2
-     * @param numbers  ����������
+     * 从第一个元素开始，该元素可以认为已经被排序
+     * 取出下一个元素，在已经排序的元素序列中从后向前扫描
+     * 如果该元素（已排序）大于新元素，将该元素移到下一位置
+     * 重复步骤3，直到找到已排序的元素小于或者等于新元素的位置
+     * 将新元素插入到该位置中
+     * 重复步骤2
+     * @param numbers  待排序数组
      */
     public static void simpleInsertSort(int[] numbers)
     {
@@ -64,7 +64,7 @@ public class SortSample {
         for(int i = 0 ; i < numbers.length ; i++)
         {
             temp = numbers[i];
-            //����temp��ǰ���ֵС����ǰ���ֵ����
+            //假如temp比前面的值小，则将前面的值后移
             for(j = i ; j > 0 &&  numbers[j-1] > temp ; j --)
             {
                 numbers[j] = numbers[j-1];
@@ -76,21 +76,21 @@ public class SortSample {
     }
 
     /*
-    1������˼�룺
-    �Ƚ�����������ļ�¼���зָ��Ϊ���������зֱ����ֱ�Ӳ������򣬴����������еļ�¼����������ʱ���ٶ�ȫ���¼��������ֱ�Ӳ�������
-    2������������
+    1、基本思想：
+    先将整个待排序的记录序列分割成为若干子序列分别进行直接插入排序，待整个序列中的记录“基本有序”时，再对全体记录进行依次直接插入排序。
+    2、操作方法：
 
     <code>
-    1��ѡ��һ����������t1��t2������tk������ti>tj��tk=1��
-    2�����������и���k�������н���k ������
-    3��ÿ�����򣬸��ݶ�Ӧ������ti�����������зָ�����ɳ���Ϊm �������У��ֱ�Ը��ӱ�����ֱ�Ӳ������򡣽���������Ϊ1 ʱ������������Ϊһ�����������������ȼ�Ϊ�������еĳ��ȡ�
+    1、选择一个增量序列t1，t2，…，tk，其中ti>tj，tk=1；
+    2、按增量序列个数k，对序列进行k 趟排序；
+    3、每趟排序，根据对应的增量ti，将待排序列分割成若干长度为m 的子序列，分别对各子表进行直接插入排序。仅增量因子为1 时，整个序列作为一个表来处理，表长度即为整个序列的长度。
     </code>
      */
     public static void shellSort(int[] data)
     {
         int j = 0;
         int temp = 0;
-        //ÿ�ν���������Ϊԭ����һ��
+        //每次将步长缩短为原来的一半
         for (int increment = data.length / 2; increment > 0; increment /= 2)
         {
             for (int i = increment; i < data.length; i++)
@@ -98,7 +98,7 @@ public class SortSample {
                 temp = data[i];
                 for (j = i; j >= increment; j -= increment)
                 {
-                    if(temp < data[j - increment])//�����С������ֻ���޸�����
+                    if(temp < data[j - increment])//如想从小到大排只需修改这里
                     {
                         data[j] = data[j - increment];
                     }
@@ -116,21 +116,21 @@ public class SortSample {
         }
     }
     /**
-     * ѡ�������㷨
-     * ��δ�����������ҵ���СԪ�أ���ŵ��������е���ʼλ��
-     * �ٴ�ʣ��δ����Ԫ���м���Ѱ����СԪ�أ�Ȼ��ŵ���������ĩβ��
-     * �Դ����ƣ�ֱ������Ԫ�ؾ�������ϡ�
+     * 选择排序算法
+     * 在未排序序列中找到最小元素，存放到排序序列的起始位置
+     * 再从剩余未排序元素中继续寻找最小元素，然后放到排序序列末尾。
+     * 以此类推，直到所有元素均排序完毕。
      * @param numbers
      */
     public static void selectSort(int[] numbers)
     {
-        int size = numbers.length; //���鳤��
-        int temp = 0 ; //�м����
+        int size = numbers.length; //数组长度
+        int temp = 0 ; //中间变量
 
         for(int i = 0 ; i < size ; i++)
         {
-            int k = i;   //��ȷ����λ��
-            //ѡ���Ӧ���ڵ�i��λ�õ���  j Ҫ��i ��ʼ����
+            int k = i;   //待确定的位置
+            //选择出应该在第i个位置的数  j 要从i 开始算起
             for(int j = i ; j < size; j++)
             {
                 if(numbers[j] < numbers[k])
@@ -138,7 +138,7 @@ public class SortSample {
                     k = j;
                 }
             }
-            //����������
+            //交换两个数
             temp = numbers[i];
             numbers[i] = numbers[k];
             numbers[k] = temp;
